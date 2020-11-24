ba013d2a232ea399ef38e217675953016f5f0136
reza rayatnia <r.rayatnia@gmail.com>
2020-11-24 23:47:17 +0330

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <product :product="product" />
+  </div>
+</template>
+
+<script>
+import Product from "./components/Product";
+
+export default {
+  name: "App",
+  components: {
+    Product,
+  },
+  data() {
+    return {
+      product: {
+        id: 0,
+        name: "سکه تمام بهار (طرح قدیم)",
+        price: 11200000,
+        changeInPercent: 0.5,
+        changeInCurrency: 200000,
+      },
+    };
+  },
+};
+</script>
---
+++ src/components/Product.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span class="mx-2">{{ product.name }}</span>
+    <span class="mx-2">{{ product.price }}</span>
+    <span class="mx-2">{{ product.changeInPercent }}</span>
+    <span class="mx-2">{{ product.changeInCurrency }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: ["product"],
+};
+</script>
